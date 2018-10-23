ce959c57e13612dfbabe404cafa7bf44cb92db4d
prashanth-sams <sams.prashanth@gmail.com>
2018-10-23 20:15:53 +0400

    new fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ export DISPLAY=:20
 Xvfb :20 -screen 0 1366x768x16 &
 x11vnc -passwd TestVNC -display :20 -N -forever &
 
+source /usr/local/rvm/scripts/rvm
+
+cucumber features/scenarios/demo/google.feature MODE=headless
 cd /usr/tests/
 rm -rf ruby_cucumber/
 git clone https://github.com/prashanth-sams/ruby_cucumber.git /usr/tests/ruby_cucumber
 cd /usr/tests/ruby_cucumber
-rvm -v
-ruby -v
-bundle -v
 bundle install
 cucumber features/scenarios/demo/google.feature MODE=headless
 wait
